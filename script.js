8a35937e8025c986ca25607dc48c4fc0b461a69e
Paul-Runge <brfirestarter@gmail.com>
2023-03-07 14:32:15 -0800

    created more semantic HTML tags and flow; added weather icons to JS; commented code

--- script.js
+++ script.js
@@ -14,16 +14,21 @@ let searchHistory = JSON.parse(localStorage.getItem('search')) || [];
 // Clear search history
 const clearHistoryBtn = document.getElementById('clear-history');
 
-weatherTodayEl = document.getElementById('weather-today');
-cityNameEl = document.getElementById('city-name');
-tempEl = document.getElementById('temperature');
-windEl = document.getElementById('wind');
-humidityEl = document.getElementById('humidity');
-fivedayEl = document.querySelectorAll('.forecast');
-// console.log(fivedayEl.length);
-// Get the current day & 5-day forecast weather data from openweathermap API using name of the desired city as parameter.
-// Currently just displaying data to console.
-// Will add code to render to app display later on.
+// Today's weather display
+let weatherTodayEl = document.getElementById('weather-today');
+let cityNameEl = document.getElementById('city-name');
+
+// 5-Day forecast weather display
+let fivedayEl = document.querySelectorAll('.forecast');
+let fiveDayCard = document.getElementById('forecast-card');
+
+// Specific weather data
+let tempEl = document.getElementById('temperature');
+let windEl = document.getElementById('wind');
+let humidityEl = document.getElementById('humidity');
+let todayIcon = document.getElementById('today-icon');
+
+// Get current day & 5-day forecast weather data from openweathermap API using name of the desired city as argument.
 function getWeather(cityName) {
     // Renders temp units in imperial instead of deafult Kelvin scale.
     // URL for current day weather
@@ -33,23 +38,26 @@ function getWeather(cityName) {
     let fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" + cityName + "&appid=" + APIKey;
     
     
-    // Get, log, and display current day weather data
+    // Get, log, and display current day weather data to a card
     fetch(todayURL)
         .then(function (response) { 
             response.json()
             .then(function (data) { 
-                console.log(data);
-                // Make element visible
+                // console.log(data);
+                // Make today's weather card visible
                 weatherTodayEl.classList.remove('d-none');
+                // Get current date and extract relevant data points
                 const date = new Date(data.dt * 1000);
-                // console.log(date);
                 const year = date.getFullYear();
-                //console.log(year);
                 const month = date.getMonth() + 1;
-                //console.log(month);
                 const day = date.getDate();
-                //console.log(day);
+                // Get icon representing current weather pattern
+                let todayIconCode = data.weather[0].icon;
+                let todayIconURL = "https://openweathermap.org/img/wn/" + todayIconCode + ".png";
+                todayIcon.setAttribute("src", todayIconURL);
+                // Format/render date and relevant weather data w/ imperial unit notation
                 cityNameEl.innerHTML = data.name + " (" + month + "/" + day + "/" + year + ")";
+                cityNameEl.append(todayIcon); // Display icon alongside city name
                 tempEl.innerHTML = "Temperature: " + Math.floor(data.main.temp) + ' \u00B0F';
                 windEl.innerHTML = "Wind: " + data.wind.speed + " mph";
                 humidityEl.innerHTML = "Humidity: " + data.main.humidity + "%";
@@ -62,27 +70,37 @@ function getWeather(cityName) {
             response.json()
             .then (function (data) {
                 // console.log(data);
+                // Make 5-day weather forecast card visible
+                fiveDayCard.classList.remove('d-none');
+                // Loop through each 5-day element giving them structural elements and relevant data points
                 for (let i = 0; i < fivedayEl.length; i++) {
+                    // Make sure each newly addressed element is initially empty
                     fivedayEl[i].innerHTML = "";
+                    // The API gives blocks of data for every 3-hours (8 blokcs per day) starting with the last block of the current day.  This index caculation initially selects the first block of the next day, and jumps forward 1 day (8 blocks) with each subsequent iteration.
                     const index = i * 8 + 1;
+                    // Get date and extract relevant data points for current index
                     const date = new Date(data.list[index].dt_txt);
-                    // console.log(date);
                     const year = date.getFullYear();
-                    //console.log(year);
                     const month = date.getMonth() + 1;
-                    //console.log(month);
                     const day = date.getDate();
-                    //console.log(day);
-                    const fiveDayDate = document.createElement('p');
+                    // Create structural elements for current index
+                    const fiveDayDate = document.createElement('h5');
+                    const fiveDayIcon = document.createElement('img');
                     const fiveDayTemp = document.createElement('p');
                     const fiveDayWind = document.createElement('p');
                     const fiveDayHumidity = document.createElement('p');
-                    // console.log(fivedayEl[index]);
+                    // Get icon matching current index weather pattern and display
+                    let fiveDayIconCode = data.list[index].weather[0].icon;
+                    let fiveDayIconURL = "https://openweathermap.org/img/wn/" + fiveDayIconCode + ".png";
+                    fiveDayIcon.setAttribute("src", fiveDayIconURL);
+                    // Format/render date and relevant weather data w/ imperial unit notation
                     fiveDayDate.innerHTML = month + "/" + day + "/" + year;
                     fiveDayTemp.innerHTML = "Temperature: " + Math.floor(data.list[index].main.temp) + ' \u00B0F';
                     fiveDayWind.innerHTML = "Wind: " + data.list[index].wind.speed + " mph";
                     fiveDayHumidity.innerHTML = "Humidity: " + data.list[index].main.humidity + "%";
+                    // Attach newly created elements to 5-day element at current index
                     fivedayEl[i].append(fiveDayDate);
+                    fivedayEl[i].append(fiveDayIcon);
                     fivedayEl[i].append(fiveDayTemp);
                     fivedayEl[i].append(fiveDayWind);
                     fivedayEl[i].append(fiveDayHumidity);
@@ -98,8 +116,8 @@ cityForm.addEventListener('submit', function (event) {
     if (!cityInput.value) {
         return;
     }
-    // Will fill out rest of code before implementing this block to avoid potential rendering issues.
     // This is intended to eliminate storing duplicate search items.
+    // Could not work out the details.  Will address this in the future.
     // for (let i = 0; i < searchHistory.length; i++) {
     //     if (cityInput.value != searchHistory[i]) {
             
@@ -107,19 +125,21 @@ cityForm.addEventListener('submit', function (event) {
     // }
     // console.log(cityInput.value);
 
-    // Call getWeather w/ user input city name as parameter
+    // Call getWeather w/ user input city name as argument
     getWeather(cityInput.value);
     // Push city name to search history array
     searchHistory.push(cityInput.value);
     // Commit city name to local storage
     localStorage.setItem('search', JSON.stringify(searchHistory));
+    // Render updated search history
     renderSearchHistory();
+    // Reset input field; ready for next entry
     cityInput.value = "";
 });
 
-// Create new element for each search history item and render it to the screen as a button.
+// Create new element for each search history item and render it to the screen as a button that will can be interacted with to perform a new search for weather data for the targeted city.
 function renderSearchHistory() {
-    // Initialize blank search history bin
+    // Initialize blank search history
     searchHistoryEl.innerHTML = "";
     // Loop through entire search history
     for (let i = 0; i < searchHistory.length; i++) {
@@ -129,11 +149,11 @@ function renderSearchHistory() {
         historyBtn.setAttribute('type', 'submit');
         historyBtn.setAttribute('readonly', true);
         historyBtn.setAttribute('class', 'btn btn-secondary btn-block mt-2');
-        // This button value matches this search history item
+        // This button value to match search history item value (city name) at current index
         historyBtn.setAttribute('value', searchHistory[i]);
-        // Give button a text value
+        // Give button a text value representing the targeted city
         historyBtn.innerHTML = historyBtn.value;
-        // Event listener for click on search history button to execute getWeather() with associated city name as parameter.
+        // Event listener for click on search history button to execute getWeather() with targeted city name as argument.
         historyBtn.addEventListener('click', function (event) {
             event.preventDefault();
             getWeather(historyBtn.value);
@@ -143,7 +163,7 @@ function renderSearchHistory() {
     }
 }
 
-// Event listener to reset local storage and search history upon click 
+// Event listener for clear history button to reset local storage and search history upon click
 clearHistoryBtn.addEventListener('click', function() {
     localStorage.clear();
     searchHistory = [];
